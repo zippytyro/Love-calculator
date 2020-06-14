efb1f2be671b64dc0f451a26a139da11edd63fda
Shashwat verma <42150187+zippytyro@users.noreply.github.com>
2020-06-14 13:22:31 +0530

    update app.js

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 const message = document.getElementById("message");
 const btn = document.getElementById("button");
 const meter = document.getElementById("loveMeter");
-
 btn.addEventListener("click", function () {
     const random = Math.floor(Math.random() * 100) + 1;
     meter.style.visibility = "visible";
-
     if (random > 0 && random < 30) {
         meter.style.backgroundColor = "red";
         message.innerHTML = "You go with each other like oil and water - " + random + "%";
@@ -14,7 +12,7 @@ btn.addEventListener("click", function () {
         message.innerHTML = "Show some more love - " + random + "%";
     } else if (random > 50 && random < 70) {
         meter.style.backgroundColor = "blue";
-        message.innerHTML = "Your relationship resembles with Chai andd biscut- " + random + "%";
+        message.innerHTML = "Your relationship resembles to Chai and biscuit - " + random + "%";
     } else {
         meter.style.backgroundColor = "green";
         message.innerHTML = "You are made for each other - " + random + "%";
